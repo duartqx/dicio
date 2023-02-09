5b1ed8b266400a0f03ed49b4b04cadaf661c05b7
Diego Duarte <duartqx@gmail.com>
2023-02-08 23:46:28 -0300

    black

--- defina_pt.py
+++ defina_pt.py
@@ -8,43 +8,44 @@ from urllib.error import HTTPError
 from urllib.request import urlopen
 
 
-class NotFoundError(Exception): pass
+class NotFoundError(Exception):
+    pass
 
 
 class Description:
-    
     def __init__(self, word) -> None:
         self.word = word
         self.nrml_word = self.normalize_word()
         self.description = self.get_description()
 
     def __repr__(self) -> str:
-        ''' Returns word capitalized + the description (that can be 'Result 
-        not found') 
+        """Returns word capitalized + the description (that can be 'Result
+        not found')
         The weird encoded characters around the word are an ANSI escape
         sequence that causes the word to be printed in the color green in the
-        terminal, instead of the default white '''
+        terminal, instead of the default white"""
         return "\n\033[1;32m{}\033[00m\n{}\n".format(
-                self.word.capitalize(), self.description)
+            self.word.capitalize(), self.description
+        )
 
     def normalize_word(self) -> str:
-        ''' normalize_word returns the word without accents, cedilha, etc
-         to be concatenated with the URL_BASE and used to get a response '''
-        return ''.join([c for c in normalize('NFKD',
-               self.word) if not combining(c)])
+        """normalize_word returns the word without accents, cedilha, etc
+        to be concatenated with the URL_BASE and used to get a response"""
+        return "".join([c for c in normalize("NFKD", self.word) if not combining(c)])
 
     def _get_result(self) -> str:
-        ''' Returns the html content of dicio.com.br + self.norm_word or raises
-        NotFoundError exception if the word was not found '''
-        URL_BASE: str = 'https://www.dicio.com.br/'
+        """Returns the html content of dicio.com.br + self.norm_word or raises
+        NotFoundError exception if the word was not found"""
+        URL_BASE: str = "https://www.dicio.com.br/"
         try:
             content: str = urlopen(URL_BASE + self.nrml_word).read().decode()
         except HTTPError:
             raise NotFoundError
-        
-        result: Optional[Match[str]] = search('<p itemprop="description" ' + \
-                                       'class=*(.*)</p>', content)
-        
+
+        result: Optional[Match[str]] = search(
+            '<p itemprop="description" ' + "class=*(.*)</p>", content
+        )
+
         # This one was to make mypy happy about None not having group()
         if result:
             return result.group()
@@ -54,24 +55,24 @@ class Description:
     def get_description(self) -> str:
         try:
             result: str = self._get_result()
-            if 'Ainda não temos o significado' in result:
+            if "Ainda não temos o significado" in result:
                 # if the word is not on dicio.com.br, result can be None, if
                 # it's gibberish, or it can be a warning that the word is not
                 # on the site if it's a word with similar results on the site
                 # Raising error to avoid returning two types (NoneType or Str)
                 raise NotFoundError
         except NotFoundError:
-            return 'Result not found'
-        
-        t = '. '.join(sub('<.*?>','',result).split('.'))
-        # to format the description first re.sub is used here to  removes 
-        # all html tags like <span> </span> <p> that it can find. 
-        # That can lead to some words being united with a dot were a tag 
+            return "Result not found"
+
+        t = ". ".join(sub("<.*?>", "", result).split("."))
+        # to format the description first re.sub is used here to  removes
+        # all html tags like <span> </span> <p> that it can find.
+        # That can lead to some words being united with a dot were a tag
         # was before, so the split('.') removes all dots and the string is
         # joined again with '. '.join()
-        # Finally split is used again on ';' to separate the description 
+        # Finally split is used again on ';' to separate the description
         # in multiple lines like it is presented on dicio.com.br's page
-        description = ';\n• '.join(l.capitalize() for l in t.split('; '))
+        description = ";\n• ".join(l.capitalize() for l in t.split("; "))
         return description
 
 
@@ -84,6 +85,6 @@ def main():
     print(Description(word))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     main()
--- wikidefina.py
+++ wikidefina.py
@@ -8,72 +8,84 @@ from urllib.parse import quote
 from urllib.request import urlopen
 
 
-class NotFoundError(HTTPError): pass
+class NotFoundError(HTTPError):
+    pass
 
 
 class DicioDefinition:
-
     def __init__(self, word: str) -> None:
         self.word: str = word.lower()
-        self.api_url: str = 'https://pt.wiktionary.org/w/rest.php/v1/page/'
+        self.api_url: str = "https://pt.wiktionary.org/w/rest.php/v1/page/"
         self.response: dict[str, str] = self._get_response()
         self.descr: str = self._get_description()
 
     def __repr__(self) -> str:
-        return f'\n\033[1;32m{self.word.title()}\n\n\033[00m{self.descr}\n'
+        return f"\n\033[1;32m{self.word.title()}\n\n\033[00m{self.descr}\n"
 
     def _get_response(self) -> dict[str, str]:
-        ''' Returns a dictionary, received by calling urlopen with self.api_url
+        """Returns a dictionary, received by calling urlopen with self.api_url
         + self.nrmlized, that contains the raw description in the 'source'
-        key.'''
+        key."""
         try:
             response = urlopen(self.api_url + quote(self.word))
             return load(response)
         except HTTPError:
-            return {'source': 'Result not Found'}
+            return {"source": "Result not Found"}
 
     def _get_description(self) -> str:
-        ''' Cleans up self.response['source'] by removing some unneeded text by
+        """Cleans up self.response['source'] by removing some unneeded text by
         splitting the string in some terms with self._splitter and making
-        substitutions with self._sub '''
-        descr: str = self.response['source']
+        substitutions with self._sub"""
+        descr: str = self.response["source"]
 
         for method in [self._splitter, self._sub]:
             descr = method(descr)
 
-        #print(repr(descr.strip()))
+        # print(repr(descr.strip()))
         return descr.strip()
 
     @staticmethod
     def _splitter(s: str) -> str:
-        ''' Splits s with re.split on all lines starting with a # and returns
+        """Splits s with re.split on all lines starting with a # and returns
         everything other than the last item on the split to remove unneeded
-        informations '''
-        return ''.join(split(r'((?:#))(.*)', s)[:-1])
+        informations"""
+        return "".join(split(r"((?:#))(.*)", s)[:-1])
 
     @staticmethod
     def _sub(s: str) -> str:
-        ''' zips patterns and replacements and calls sub with then to clean up
-        s '''
-        patterns: list[str] = [ 
-            '\|',
-            r'\*.*|Notas.*|\-pt\-|\<(.*?)\>|wiki(.*?)\:|',
-            r'({{Wiki|Imagem|ver também)(.*?)\n',
-            '=+|  |\{\{\}\}|\{\{$',
-            '\n\n+',
-            r'(?:#)(.*)',
-            '\[\[|\:\'\'\'',
-            '\{\{',
-            '\]\]|\}\}|\'\'\'',
-            r'(?:==+)(.*?)(?:==+)',
-            '\x1b\[1m\x1b\[3m\n\n',
-            '⚫($|\n+)', '\'\'',
-            '\x1b\[1m(\n+)|\x1b\[3m(\n+)|(\n+)\x1b\[1m', 
+        """zips patterns and replacements and calls sub with then to clean up
+        s"""
+        patterns: list[str] = [
+            "\|",
+            r"\*.*|Notas.*|\-pt\-|\<(.*?)\>|wiki(.*?)\:|",
+            r"({{Wiki|Imagem|ver também)(.*?)\n",
+            "=+|  |\{\{\}\}|\{\{$",
+            "\n\n+",
+            r"(?:#)(.*)",
+            "\[\[|\:'''",
+            "\{\{",
+            "\]\]|\}\}|'''",
+            r"(?:==+)(.*?)(?:==+)",
+            "\x1b\[1m\x1b\[3m\n\n",
+            "⚫($|\n+)",
+            "''",
+            "\x1b\[1m(\n+)|\x1b\[3m(\n+)|(\n+)\x1b\[1m",
         ]
         replacements: list[str] = [
-            ' | ', '', '', '', '\n\n', r'⚫\1', 
-            '\033[1m', '\033[3m', '\033[00m', 
-            r'\033[3m\1\033[00m\n', '', '\n', '\"', '',
+            " | ",
+            "",
+            "",
+            "",
+            "\n\n",
+            r"⚫\1",
+            "\033[1m",
+            "\033[3m",
+            "\033[00m",
+            r"\033[3m\1\033[00m\n",
+            "",
+            "\n",
+            '"',
+            "",
         ]
 
         for pattern, repl in zip(patterns, replacements):
@@ -85,12 +97,15 @@ class DicioDefinition:
 
 
 def main() -> None:
-    ''' Calls the DicioDefinition class if a word is passed as argv[1] '''
-    try: word: str = argv[1]
-    except IndexError: print('\nNo search word provided\n'); return
+    """Calls the DicioDefinition class if a word is passed as argv[1]"""
+    try:
+        word: str = argv[1]
+    except IndexError:
+        print("\nNo search word provided\n")
+        return
     print(DicioDefinition(word))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     main()
